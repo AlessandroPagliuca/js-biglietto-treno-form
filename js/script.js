d4357fa01f80928d9d3a720c1b42a9ca8a3e33bd
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-09 14:59:30 +0100

    Start project

---
+++ js/script.js
@@ -0,0 +1,65 @@
+/*
+Descrizione:
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo due caselle di input e almeno un bottone per inviare i dati in pagina, in cui l’utente potrà inserire i dati e poi visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+const numberKm
+const passengerAge
+let priceTicket (0.21€)
+const discountUnderage  (*0.2)
+const discountOverSixtyFive (*0.4)
+let priceFinal in output
+*/
+
+//const numberKm in input prompt
+const numberKm = prompt("Enter the distance to travel in km");
+
+//const passenger in input prompt
+const passengerAge= prompt("Please enter your age");
+
+//let priceTicket
+let priceTicket = numberKm * 0.21;
+
+//const discountUnderage
+const discountUnderage = 18;
+
+//const discountOverSixtyFive
+const discountOverSixtyFive = 65;
+
+//let messageDiscount
+let messageDiscount;
+
+//Apply discount for Underage and  OverSixtyFive
+if(!isNaN(numberKm) && !isNaN(passengerAge)){
+    if(passengerAge < discountUnderage){
+        priceTicket *= 0.2;
+        messageDiscount = 'The discount has been applied to your ticket,' + 'thank you for choosing us:' + ' ' + priceTicket.toFixed(2) + '€';
+    } else if(passengerAge >= discountOverSixtyFive){
+        priceTicket *= 0.4;
+        messageDiscount = 'The discount has been applied to your ticket,' + 'thank you for choosing us:' + ' ' + priceTicket.toFixed(2) + '€';
+    } else{
+        messageDiscount = 'No discount has been applied to your ticket,' + ' ' + priceTicket.toFixed(2) + '€';
+    }
+
+} else{
+    messageDiscount = "Invalid data entered, please try again";
+}
+
+
+
+//print the priceFinal in human format with two decimals
+let priceFinal = messageDiscount;
+console.log(priceFinal);
+
+document.getElementById("ticket").innerHTML = priceFinal;
